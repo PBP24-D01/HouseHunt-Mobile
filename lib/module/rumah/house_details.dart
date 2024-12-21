8d7e339118b289ab17b99dd7c2ad463f74aa86f0
Alvin <imoonfellow@gmail.com>
2024-12-21 22:47:26 +0700

    added edit

--- lib/module/rumah/house_details.dart
+++ lib/module/rumah/house_details.dart
@@ -1,16 +1,47 @@
 import 'package:flutter/material.dart';
 import 'package:househunt_mobile/module/rumah/models/house.dart';
+import 'package:househunt_mobile/module/rumah/edit_house.dart';
+import 'package:househunt_mobile/module/auth/login.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-class HouseDetailsPage extends StatelessWidget {
+class HouseDetailsPage extends StatefulWidget {
   final House house;
 
   const HouseDetailsPage({Key? key, required this.house}) : super(key: key);
 
   @override
+  _HouseDetailsPageState createState() => _HouseDetailsPageState();
+}
+
+class _HouseDetailsPageState extends State<HouseDetailsPage> {
+  bool isSeller = false;
+  bool isBuyer = false;
+  bool isAuthenticated = false;
+
+  @override
+  void initState() {
+    super.initState();
+    checkUserType();
+  }
+
+  Future<void> checkUserType() async {
+    final prefs = await SharedPreferences.getInstance();
+    final isBuyer = prefs.getBool('is_buyer') ?? false;
+    final isSeller = !isBuyer;
+    final isAuthenticated = prefs.getBool('is_authenticated') ?? false;
+
+    setState(() {
+      this.isSeller = isSeller;
+      this.isBuyer = isBuyer;
+      this.isAuthenticated = isAuthenticated;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(house.title),
+        title: Text(widget.house.title),
       ),
       body: SingleChildScrollView(
         padding: const EdgeInsets.all(16.0),
@@ -23,15 +54,15 @@ class HouseDetailsPage extends StatelessWidget {
               height: 300, // Set a fixed height for the image container
               decoration: BoxDecoration(
                 color: Colors.grey[300],
-                image: house.imageUrl != null
+                image: widget.house.imageUrl != null
                     ? DecorationImage(
-                        image: NetworkImage(house.imageUrl!),
+                        image: NetworkImage(widget.house.imageUrl!),
                         fit: BoxFit
                             .contain, // Scale the image to fit within the container
                       )
                     : null,
               ),
-              child: house.imageUrl == null
+              child: widget.house.imageUrl == null
                   ? const Icon(
                       Icons.image,
                       size: 100,
@@ -41,7 +72,7 @@ class HouseDetailsPage extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Text(
-              house.title,
+              widget.house.title,
               style: const TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
@@ -49,7 +80,7 @@ class HouseDetailsPage extends StatelessWidget {
             ),
             const SizedBox(height: 8),
             Text(
-              house.location,
+              widget.house.location,
               style: const TextStyle(
                 fontSize: 18,
                 color: Colors.grey,
@@ -57,7 +88,7 @@ class HouseDetailsPage extends StatelessWidget {
             ),
             const SizedBox(height: 8),
             Text(
-              'Rp ${house.price}',
+              'Rp ${widget.house.price}',
               style: const TextStyle(
                 fontSize: 20,
                 fontWeight: FontWeight.bold,
@@ -66,7 +97,7 @@ class HouseDetailsPage extends StatelessWidget {
             ),
             const SizedBox(height: 16),
             Text(
-              house.description,
+              widget.house.description,
               style: const TextStyle(
                 fontSize: 16,
               ),
@@ -76,23 +107,53 @@ class HouseDetailsPage extends StatelessWidget {
               children: [
                 const Icon(Icons.bed),
                 const SizedBox(width: 5),
-                Text('${house.bedrooms} Bedrooms'),
+                Text('${widget.house.bedrooms} Bedrooms'),
                 const SizedBox(width: 20),
                 const Icon(Icons.bathtub),
                 const SizedBox(width: 5),
-                Text('${house.bathrooms} Bathrooms'),
+                Text('${widget.house.bathrooms} Bathrooms'),
               ],
             ),
             const SizedBox(height: 16),
             Text(
-              house.isAvailable ? 'Available' : 'Not Available',
+              widget.house.isAvailable ? 'Available' : 'Not Available',
               style: TextStyle(
                 fontSize: 16,
-                color: house.isAvailable ? Colors.green : Colors.red,
+                color: widget.house.isAvailable ? Colors.green : Colors.red,
               ),
             ),
           ],
         ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: isSeller
+            ? ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => EditHousePage(house: widget.house),
+                    ),
+                  );
+                },
+                child: const Text('Edit House'),
+              )
+            : ElevatedButton(
+                onPressed: () {
+                  if (isAuthenticated) {
+                    // Implement buy functionality
+                  } else {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => const LoginPage(),
+                      ),
+                    );
+                  }
+                },
+                child: const Text('Buy House'),
+              ),
       ),
     );
   }
