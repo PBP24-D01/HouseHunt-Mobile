2621104fc613d2e1bddb5990831f48800e68fee7
favianzhafif26 <favianzhafif26@gmail.com>
2024-12-22 17:44:27 +0700

    making crud for flutter (not done)

--- lib/module/iklan/main.dart
+++ lib/module/iklan/main.dart
@@ -221,7 +221,6 @@ class _IklanPageState extends State<IklanPage> {
       appBar: AppBar(
         title: Text('Iklan List'),
       ),
-<<<<<<< HEAD
       drawer: LeftDrawer(), // Assuming you have a DrawerWidget
       body: FutureBuilder<List<Iklan>>(
         future: fetchIklan(context.read<CookieRequest>()),
@@ -264,111 +263,6 @@ class _IklanPageState extends State<IklanPage> {
       floatingActionButton: FloatingActionButton(
         onPressed: () => showCreateDialog(context),
         child: Icon(Icons.add),
-=======
-      drawer: const LeftDrawer(),
-      bottomNavigationBar: CustomBottomNavigationBar(currentIndex: 3),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          // Use this to make the content scrollable
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              FutureBuilder<List<Iklan>>(
-                future: fetchIklan(request),
-                builder: (context, snapshot) {
-                  if (snapshot.connectionState == ConnectionState.waiting) {
-                    return const Center(child: CircularProgressIndicator());
-                  } else if (snapshot.hasError) {
-                    return Center(
-                      child: Text('Error: ${snapshot.error}'),
-                    );
-                  } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-                    return const Center(
-                      child: Text('No Auction available'),
-                    );
-                  } else {
-                    return Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        const Text(
-                          'Auction',
-                          style: TextStyle(
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        const SizedBox(height: 12.0),
-                        SizedBox(
-                          height: 270,
-                          child: ListView.builder(
-                            scrollDirection: Axis.horizontal,
-                            itemCount: snapshot.data!.length,
-                            itemBuilder: (context, index) {
-                              final iklan = snapshot.data![index];
-                              return Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Card(
-                                  elevation: 8,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12.0),
-                                  ),
-                                  child: Padding(
-                                    padding: const EdgeInsets.all(8.0),
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          iklan.houseTitle,
-                                          overflow: TextOverflow.ellipsis,
-                                          style: const TextStyle(
-                                            fontSize: 18.0,
-                                            fontWeight: FontWeight.bold,
-                                          ),
-                                        ),
-                                        const SizedBox(height: 8.0),
-                                        ElevatedButton(
-                                          onPressed: () {
-                                            //Navigator.push(
-                                            //  context,
-                                            //  MaterialPageRoute(
-                                            //    builder: (context) =>
-                                            //        DetailProductPage(
-                                            //            keyboard: keyboard),
-                                            //  ),
-                                            //);
-                                          },
-                                          style: ElevatedButton.styleFrom(
-                                            backgroundColor:
-                                                Colors.amber, // Button color
-                                            padding: const EdgeInsets.symmetric(
-                                                horizontal: 20, vertical: 10),
-                                          ),
-                                          child: const Text(
-                                            'Detail',
-                                            style: TextStyle(
-                                                fontSize: 16,
-                                                color: Colors.white),
-                                          ),
-                                        )
-                                      ],
-                                    ),
-                                  ),
-                                ),
-                              );
-                            },
-                          ),
-                        ),
-                      ],
-                    );
-                  }
-                },
-              ), // Add spacing between sections
-            ],
-          ),
-        ),
->>>>>>> b756cb332e72634651f3402212dc991ebc9de9c2
       ),
     );
   }
